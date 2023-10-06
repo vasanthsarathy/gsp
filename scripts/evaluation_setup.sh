d35cd617b6e154d2da0a554c818f89826a9d0fad
Vasanth Sarathy <vsarathy@gmail.com>
2023-10-05 20:27:10 -0400

    scripts

---
+++ scripts/evaluation_setup.sh
@@ -0,0 +1,10 @@
+pip install --upgrade transformers
+pip install git+https://github.com/huggingface/peft.git
+pip install bitsandbytes
+pip install accelerate
+pip install datasets
+pip install evaluate
+pip install lanchain
+pip install openai
+pip install networkx
+pip install matplotlib
