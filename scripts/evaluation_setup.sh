93555a340fdddd87f848105c1a163b656553cd6b
Vasanth Sarathy <vsarathy@gmail.com>
2023-10-06 12:04:49 -0400

    fixing evaluation script

--- scripts/evaluation_setup.sh
+++ scripts/evaluation_setup.sh
@@ -4,7 +4,7 @@ pip install bitsandbytes
 pip install accelerate
 pip install datasets
 pip install evaluate
-pip install lanchain
+pip install langchain
 pip install openai
 pip install networkx
 pip install matplotlib
